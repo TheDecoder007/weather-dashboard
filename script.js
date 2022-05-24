04f0ace1572d389e81290c2ad74bedd1b5f03757
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-24 15:42:43 -0400

    got api working for todays weather

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ var cityFiveDay = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid=
 var cityCurrent = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial';
 
 //Variables from module 6. Replace classes and reconfigure to the city name button
-var userFormEl = document.querySelector('#user-form');
-var nameInputEl = document.querySelector('#username');
+//was userFormEl
+var searchFormEl = document.querySelector('#searchForm');
+//was nameInputEl
+var searchInputEl = document.querySelector('#searchInput');
 var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+//was repoSearchTerm
+var titleText = document.querySelector("#titleText");
 
 //CODE FROM MODULE 6. SEARCH INPUT creates a div and pulls info from API//
 //need to refigure, to display search info in the cards, and create a 
@@ -21,75 +24,59 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 // search bar input
 var formSubmitHandler = function(event) {
     event.preventDefault();
-  //gets value from nameInputEl
-    var username = nameInputEl.value.trim();
+  //gets value from searchInputEl
+    var cityName = searchInputEl.value.trim();
   
-      if (username) {
-        getUserRepos(username);
+      if (cityName) {
+        getCity (cityName);
   //clears the form after search
-        nameInputEl.value = "";
+        searchInputEl.value = "";
       } else {
-        alert("Pleae enter a gitHub username");
+        alert("Please enter valid city.");
       }
     console.log(event);
   };
   
   
   //function to fetch from API
-  var getUserRepos = function(user) {
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  var getCity  = function(city) {
+    var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a732740e9afd7858896a61e89013252f&units=imperial";
     
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrlCurrent).then(function(response) {
+      if (response.ok) {
         console.log(response);
           response.json().then(function(data) { 
-  //when response data is converted to JSON, it gets sent from getUserRepos to displayRepos
-              displayRepos(data, user);
+  //when response data is converted to JSON, it gets sent from getCity  to displayCity
+              displayCity(data, city);
               console.log(data);
             });
+          }  else {
+            alert("Please search for a valid city.");
+          }
       });
   }; 
   
   //accepts the array of repository data, and the term we searched for as parameters
-  var displayRepos = function(repos, searchTerm) {
+  var displayCity = function(city, searchTerm) {
   //clear old content from repos display
-  repoContainerEl.textContent = "";
-  repoSearchTerm.textContent = searchTerm;
-    console.log(repos);
+  titleText.textContent = "";
+  titleText.textContent = city.name;
+    console.log(city);
     console.log(searchTerm);
-  
-  //loop over repos
-  for (var i = 0; i < repos.length; i++) {
-  //format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
-  //create a container for each repo
-  var repoEl = document.createElement("div");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
-  //create a span element to hold repo name
-  var titleEl = document.createElement("span");
-  titleEl.textContent = repoName;
-  //append to container repoEl
-  repoEl.appendChild(titleEl);
-  
-  // create a status element
-  var statusEl = document.createElement("span");
-  statusEl.classList = "flex-row align-center";
-  
-  // check if current repo has issues or not
-  if (repos[i].open_issues_count > 0) {
-    statusEl.innerHTML =
-      "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-  } else {
-    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+
+    var todayDescEl = document.querySelector('#todayDesc');
+    todayDescEl.textContent = city.weather[0].description;
+
+    var tempEl = document.querySelector("#temp");
+    tempEl.textContent = 'Temp: ' + city.main.temp + 'F';
+
+    var windEl = document.querySelector("#wind");
+    windEl.textContent = 'Wind: ' + city.wind.speed + 'mph';
+
+    var humidityEl = document.querySelector("#humidity");
+    humidityEl.textContent = 'Humidity: ' + city.main.humidity + '%';
   }
-  // append to container repoEl
-  repoEl.appendChild(statusEl);
-  //append container to the dom
-  repoContainerEl.appendChild(repoEl);
-  }
-  //end for loop
-  };
+ 
   
   
-  
-  userFormEl.addEventListener("submit", formSubmitHandler);
-//END CODE FROM MODULE 6, lesson 6.2
+  searchFormEl.addEventListener("submit", formSubmitHandler);
